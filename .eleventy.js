fda02114c670d7323920f94443212a396eed12dc
rawywebsite <120719621+rawywebsite@users.noreply.github.com>
2022-12-16 14:38:37 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,11 @@
+const { DateTime } = require("luxon")
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/assets')
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).
+    toLocaleString(DateTime.DATE_MED);
+  })
     return {
       dir: {
         input: "src",
